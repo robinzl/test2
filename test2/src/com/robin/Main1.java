e4a32facc54866fadedf8264c2418e719bc698f4
Robin <Robin@Aling-PC>
2017-03-02 14:30:54 +0800

    第1次提交

---
+++ test2/src/com/robin/Main1.java
@@ -0,0 +1,8 @@
+package com.robin;
+
+public class Main1 {
+	public static void main(String[] args) {
+		System.out.println("test git git remote github");
+		System.out.println("test git git remote github");
+	}
+}
